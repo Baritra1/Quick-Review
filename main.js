24163cb6c0f1f0e8ee9700cfbb8e83a33269ac89
Baritra1 <aritraandbanerjee@gmail.com>
2020-09-17 16:54:24 -0500

    Tweaked end of Quick Review to work better

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-cookiechecker = localStorage.getItem("nonextbuttoncookie");
+cookiechecker = localStorage.getItem('nonextbuttoncookie')
+var entriesnumberchecker = document.getElementsByTagName('h4')[2].childNodes[2].textContent
+var entriesnumber = JSON.parse(JSON.parse(JSON.stringify(entriesnumberchecker)).split(" ",2)[0]-1)
+var result = 0
 if (cookiechecker == null) {
-  for (i = 0; i < document.getElementsByClassName("entry-text").length; i++) {
-    window.alert(document.getElementsByClassName("entry-text")[i].innerHTML);
-  }
-  if ((document.getElementsByClassName("entry-text")[i] = "undefined")) {
-    var result = 31;
-    goto_results("result-list-desktop", result);
-    localStorage.setItem("nonextbuttoncookie", result);
-  }
-} else {
-  result = localStorage.getItem("nonextbuttoncookie");
-  JSON.parse(result);
-  for (i = 0; i < document.getElementsByClassName("entry-text").length; i++) {
-    window.alert(document.getElementsByClassName("entry-text")[i].innerHTML);
-  }
-  if ((document.getElementsByClassName("entry-text")[i] = "undefined")) {
-    var result = JSON.parse(result) + 31;
-    goto_results("result-list-desktop", result);
-    localStorage.setItem("nonextbuttoncookie", result);
-  }
+for (i=0;i<document.getElementsByClassName("entry-text").length;i++) {
+window.alert(document.getElementsByClassName("entry-text")[i].innerHTML)
+result = JSON.parse(result)+1
+}
+if (document.getElementsByClassName("entry-text")[i] = "undefined" && result<entriesnumber) {
+result = JSON.parse(result)+1
+goto_results('result-list-desktop', result)
+localStorage.setItem("nonextbuttoncookie", result)
+}
+}
+else {
+result = localStorage.getItem("nonextbuttoncookie")
+JSON.parse(result)
+for (i=0;i<document.getElementsByClassName("entry-text").length;i++) {
+window.alert(document.getElementsByClassName("entry-text")[i].innerHTML)
+result = JSON.parse(result)+1
+}
+if (document.getElementsByClassName("entry-text")[i] = "undefined" && result<entriesnumber) {
+result = JSON.parse(result)+1
+goto_results('result-list-desktop', result)
+localStorage.setItem("nonextbuttoncookie", result)
+}
 }
