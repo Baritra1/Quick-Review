9d357c6a5ba684123e315deda083b939eaaff2c7
Baritra1 <aritraandbanerjee@gmail.com>
2020-09-22 21:23:05 -0500

    Allow user to use Quick Review by chaining multiple queries together (Alpha v2.8.7)

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ if (navigator.cookieEnabled == false) {
     speed = window.prompt("Do you want to see each word or each page?(type word or page, or \n/commands for more info)","word/page")
     eval("localStorage.setItem('speed"+document.getElementById("search-word").value+"', speed)")
     }
-    if (speed == "word") {
+    switch(speed) {
+        case "word":
     cookiechecker = eval("localStorage.getItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
     var entriesnumberchecker = document.getElementsByTagName('h4')[2].childNodes[2].textContent
     var entriesnumber = JSON.parse(JSON.parse(JSON.stringify(entriesnumberchecker)).split(" ",2)[0]-1)
@@ -22,14 +23,14 @@ if (navigator.cookieEnabled == false) {
     eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     window.alert("Quick Review Aborted Successfully!");
-    throw('(Not A Real Error)Quick Review Aborted Successfully');
+    throw('(Not A Real Error)Quick Review Aborted Successfully!');
     //This isn't really an error...it's just an easy way to stop JS execution.//
     }
     }
     if (document.getElementsByClassName("entry-text")[i] = "undefined" && result<entriesnumber) {
     result = JSON.parse(result)+1
     goto_results('result-list-desktop', result)
-    document.getElementById("search-word").style.display = "none"
+    document.getElementsByClassName("main-search")[0].style.display = "none"
     eval("localStorage.setItem('nonextbuttoncookie"+document.getElementById("search-word").value+"', result)")
     }
     }
@@ -43,13 +44,13 @@ if (navigator.cookieEnabled == false) {
     eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     window.alert("Quick Review Aborted Successfully!");
-    throw('(Not A Real Error)Quick Review Aborted');
+    throw('(Not A Real Error)Quick Review Aborted Successfully!');
     //This isn't really an error...it's just an easy way to stop JS execution.//
     }
     }
     if (document.getElementsByClassName("entry-text")[i] = "undefined" && result<entriesnumber) {
     goto_results('result-list-desktop', result)
-    document.getElementById("search-word").style.display = "none"
+    document.getElementsByClassName("main-search")[0].style.display = "none"
     eval("localStorage.setItem('nonextbuttoncookie"+document.getElementById("search-word").value+"', result)")
     }
     }
@@ -58,7 +59,9 @@ if (navigator.cookieEnabled == false) {
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     window.alert("Quick Review Complete!")
     }
-    } else if (speed == "page") {
+    break;
+        case "page":
+    if (location.pathname == "/advanced-search.php") {
     var cookiechecker = eval("localStorage.getItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
     var entriesnumberchecker = document.getElementsByTagName('h4')[2].childNodes[2].textContent
     var entriesnumber = JSON.parse(JSON.parse(JSON.stringify(entriesnumberchecker)).split(" ",2)[0]-1)
@@ -67,48 +70,49 @@ if (navigator.cookieEnabled == false) {
     var abort2 = true
     var abort3 = true
     if (cookiechecker == null) {
-    for (i=0;i<30;i++) {
+    for (i=0;i<document.getElementsByClassName("entry-text").length;i++) {
     eval("var confirmcontents"+[i]+" = \"\"")
     eval("var confirmcontents"+[i]+" = confirmcontents"+[i]+".concat(\'document.getElementsByClassName(\`entry-text\`)["+[i]+"].innerHTML\')")
     eval("wordres"+[i]+" = eval(confirmcontents"+[i]+")")
+    result = JSON.parse(result)+1
     }
-    abort2 = window.confirm("Page "+((cookiechecker/31)+1)+":\n"+wordres0+", "+wordres1+", "+wordres2+", "+wordres3+", "+wordres4+", "+wordres5+", "+wordres6+", "+wordres7+", "+wordres8+", "+wordres9+", "+wordres10+", "+wordres11+", "+wordres12+", "+wordres13+", "+wordres14+", "+wordres15+", "+wordres16+", "+wordres17+", "+wordres18+", "+wordres19+", "+wordres20+", "+wordres21+", "+wordres22+", "+wordres23+", "+wordres24+", "+wordres25+", "+wordres26+", "+wordres27+", "+wordres28+", "+wordres29)
-    result = JSON.parse(result)+31
+    abort2 = window.confirm("Page 1"+":\n"+wordres0+", "+wordres1+", "+wordres2+", "+wordres3+", "+wordres4+", "+wordres5+", "+wordres6+", "+wordres7+", "+wordres8+", "+wordres9)
+    result = JSON.parse(result)+1
     if (abort2 == false || abort3 == false) {
     eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     window.alert("Quick Review Aborted Successfully!");
-    throw new Error('(Not A Real Error)Quick Review Aborted Successfully');
+    throw new Error('(Not A Real Error)Quick Review Aborted Successfully!');
     //This isn't really an error...it's just an easy way to stop JS execution.//
     }
     if (result<entriesnumber) {
     goto_results('result-list-desktop', result)
-    document.getElementById("search-word").style.display = "none"
+    document.getElementsByClassName("main-search")[0].style.display = "none"
     eval("localStorage.setItem('nonextbuttoncookie"+document.getElementById("search-word").value+"', result)")
     }
     }
     else {
     result = eval("localStorage.getItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
-    for (i=0;i<30;i++) {
+    for (i=0;i<document.getElementsByClassName("entry-text").length;i++) {
     eval("var confirmcontents"+[i]+" = \"\"")
     eval("var confirmcontents"+[i]+" = confirmcontents"+[i]+".concat(\'document.getElementsByClassName(\`entry-text\`)["+[i]+"].innerHTML\')")
     eval("wordres"+[i]+" = eval(confirmcontents"+[i]+")")
+    result = JSON.parse(result)+1
     }
-    if (result >= 31) {
-    resultexcep = (((JSON.parse(result)-1)/30)+1)
+    if (result >= 11) {
+    resultexcep = (((JSON.parse(result)-1)/10))
     }
-    abort3 = window.confirm("Page "+(resultexcep)+":\n"+wordres0+", "+wordres1+", "+wordres2+", "+wordres3+", "+wordres4+", "+wordres5+", "+wordres6+", "+wordres7+", "+wordres8+", "+wordres9+", "+wordres10+", "+wordres11+", "+wordres12+", "+wordres13+", "+wordres14+", "+wordres15+", "+wordres16+", "+wordres17+", "+wordres18+", "+wordres19+", "+wordres20+", "+wordres21+", "+wordres22+", "+wordres23+", "+wordres24+", "+wordres25+", "+wordres26+", "+wordres27+", "+wordres28+", "+wordres29)
+    abort3 = window.confirm("Page "+(resultexcep)+":\n"+wordres0+", "+wordres1+", "+wordres2+", "+wordres3+", "+wordres4+", "+wordres5+", "+wordres6+", "+wordres7+", "+wordres8+", "+wordres9)
     if (abort2 == false || abort3 == false) {
     eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     window.alert("Quick Review Aborted Successfully!");
-    throw new Error('(Not A Real Error)Quick Review Aborted Successfully');
+    throw new Error('(Not A Real Error)Quick Review Aborted Successfully!');
     //This isn't really an error...it's just an easy way to stop JS execution.//
     }
     if (result<entriesnumber) {
-    result = JSON.parse(result)+30
     goto_results('result-list-desktop', result)
-    document.getElementById("search-word").style.display = "none"
+    document.getElementsByClassName("main-search")[0].style.display = "none"
     eval("localStorage.setItem('nonextbuttoncookie"+document.getElementById("search-word").value+"', result)")
     }
     }
@@ -117,22 +121,87 @@ if (navigator.cookieEnabled == false) {
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     window.alert("Quick Review Complete!")
     }
-    } else if (speed == "/changelog") {
+    
+    } else{
+    var cookiechecker = eval("localStorage.getItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
+    var entriesnumberchecker = document.getElementsByTagName('h4')[2].childNodes[2].textContent
+    var entriesnumber = JSON.parse(JSON.parse(JSON.stringify(entriesnumberchecker)).split(" ",2)[0]-1)
+    var result = 0
+    var resultexcep = 0
+    var abort2 = true
+    var abort3 = true
+    if (cookiechecker == null) {
+    for (i=0;i<document.getElementsByClassName("entry-text").length;i++) {
+    eval("var confirmcontents"+[i]+" = \"\"")
+    eval("var confirmcontents"+[i]+" = confirmcontents"+[i]+".concat(\'document.getElementsByClassName(\`entry-text\`)["+[i]+"].innerHTML\')")
+    eval("wordres"+[i]+" = eval(confirmcontents"+[i]+")")
+    result = JSON.parse(result)+1
+    }
+    abort2 = window.confirm("Page "+(resultexcep)+":\n"+wordres0+", "+wordres1+", "+wordres2+", "+wordres3+", "+wordres4+", "+wordres5+", "+wordres6+", "+wordres7+", "+wordres8+", "+wordres9+", "+wordres10+", "+wordres11+", "+wordres12+", "+wordres13+", "+wordres14+", "+wordres15+", "+wordres16+", "+wordres17+", "+wordres18+", "+wordres19+", "+wordres20+", "+wordres21+", "+wordres22+", "+wordres23+", "+wordres24+", "+wordres25+", "+wordres26+", "+wordres27+", "+wordres28+", "+wordres29)
+    result = JSON.parse(result)+1
+    if (abort2 == false || abort3 == false) {
+    eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
+    eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
+    window.alert("Quick Review Aborted Successfully!");
+    throw new Error('(Not A Real Error)Quick Review Aborted Successfully!');
+    //This isn't really an error...it's just an easy way to stop JS execution.//
+    }
+    if (result<entriesnumber) {
+    goto_results('result-list-desktop', result)
+    document.getElementsByClassName("main-search")[0].style.display = "none"
+    eval("localStorage.setItem('nonextbuttoncookie"+document.getElementById("search-word").value+"', result)")
+    }
+    } else {
+    result = eval("localStorage.getItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
+    for (i=0;i<document.getElementsByClassName("entry-text").length;i++) {
+    eval("var confirmcontents"+[i]+" = \"\"")
+    eval("var confirmcontents"+[i]+" = confirmcontents"+[i]+".concat(\'document.getElementsByClassName(\`entry-text\`)["+[i]+"].innerHTML\')")
+    eval("wordres"+[i]+" = eval(confirmcontents"+[i]+")")
+    result = JSON.parse(result)+1
+    }
+    if (result >= 31) {
+    resultexcep = (((JSON.parse(result)-1)/30))
+    }
+    abort3 = window.confirm("Page "+(resultexcep)+":\n"+wordres0+", "+wordres1+", "+wordres2+", "+wordres3+", "+wordres4+", "+wordres5+", "+wordres6+", "+wordres7+", "+wordres8+", "+wordres9+", "+wordres10+", "+wordres11+", "+wordres12+", "+wordres13+", "+wordres14+", "+wordres15+", "+wordres16+", "+wordres17+", "+wordres18+", "+wordres19+", "+wordres20+", "+wordres21+", "+wordres22+", "+wordres23+", "+wordres24+", "+wordres25+", "+wordres26+", "+wordres27+", "+wordres28+", "+wordres29)
+    if (abort2 == false || abort3 == false) {
+    eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
+    eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
+    window.alert("Quick Review Aborted Successfully!");
+    throw new Error('(Not A Real Error)Quick Review Aborted Successfully!');
+    //This isn't really an error...it's just an easy way to stop JS execution.//
+    }
+    if (result<entriesnumber) {
+    goto_results('result-list-desktop', result)
+    document.getElementsByClassName("main-search")[0].style.display = "none"
+    eval("localStorage.setItem('nonextbuttoncookie"+document.getElementById("search-word").value+"', result)")
+    }
+    }
+    if (result>=entriesnumber) {
+    eval("localStorage.removeItem('nonextbuttoncookie"+document.getElementById("search-word").value+"')")
+    eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
+    window.alert("Quick Review Complete!")
+    }
+    }
+    break;
+        case "/changelog":
     window.alert("You can find the official Quick Review Changelog here:\nrb.gy/sv2qwb")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
-    } else if (speed == "/commands") {
+    break;
+        case "/commands":
     window.alert("These are the current commands:\n1.word\n2.page\n3./changelog\n4./commands\n5./blockedcookieinfo\n6./credits")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
-    } else if (speed == "/credits") {
+    break;
+        case "/credits":
     window.alert("Credits:\nProgrammer: Aritra Banerjee\nAlpha Tester: Aaron Chang")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
-    } else if (speed == "/blockedcookieinfo") {
+    break;
+        case "/blockedcookieinfo":
     window.alert("Chrome Users: Click the three dots on the top right, then click Settings, then click on the \"Privacy & Security\" tab on the left side, click on \"Cookies and other site data\" and set it to \"Allow all cookies\".\n\nSafari Users: Click on the Safari Tab on the top left of your screen, click Preferences on the Drop-Down menu, then click on the Privacy tab and unselect the option for \"Block all cookies\".\n\nFirefox Users: Click the menu button and select options, then select the \"Privacy & Security\" tab on the left and check the option for \"Standard\".")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
-    } else if (speed == null) {
+        case speed == null:
     window.alert("Quick Review Aborted Successfully!")
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
-    } else{
+        default:
     window.alert("That is not an option. Try again.(Note: Put a forward-slash before all commands except for word and page.)");
     eval("localStorage.removeItem('speed"+document.getElementById("search-word").value+"')")
     }
